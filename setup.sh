90634843a50920c5031cb5a6e1620cdfa1ecd22c
Kevin Dong <kevindong@users.noreply.github.com>
2016-11-03 21:37:01 -0400

    Add personal machine setup instructions

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get install zsh vim gdb valgrind git curl
+curl -L http://install.ohmyz.sh | sh
+chsh -s /bin/zsh
+vim ~/.zshrc
+curl -L kevindong.net/vimrc -o ~/.vimrc
+ssh-keygen -t rsa -b 4096
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+ssh-copy-id dong70@data.cs.purdue.edu
